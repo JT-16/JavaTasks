dfa1ec4c96e30a89bda46432e30946b939e9f6a2
JT-901 <blackbarrel80@gmail.com>
2026-02-10 23:58:01 +0100

    first commit

---
+++ src/LetterTask.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class LetterTask {
+    public static void main(String[] args) {
+        String message = "";
+        System.out.println("My name is Jethro!");;
+    }
+}
