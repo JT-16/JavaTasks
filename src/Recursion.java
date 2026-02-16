395df2bbbc8cd9fb0d5e4cbb24ddd6aee4a42876
JT-901 <blackbarrel80@gmail.com>
2026-02-16 20:48:09 +0100

    third commit

---
+++ src/Recursion.java
@@ -0,0 +1,18 @@
+public class Recursion {
+    public static int factorial(int n) {
+        if (n >= 1) {
+            return n * factorial(n - 1);
+        } else {
+            return 1;
+        }
+    }
+
+
+
+
+
+        public static void main(String[] args) {
+            System.out.println("Factorial of 5 is " + factorial(5));
+        }
+      }
+
---
+++ src/WelcomeMessage.java
@@ -0,0 +1,10 @@
+public class WelcomeMessage {
+    static void welcomeMessage(String name) {
+        System.out.println("Welcome to Java, " + name);
+    }
+
+    public static void main(String[] args) {
+        welcomeMessage("Jethro");
+    }
+}
+
